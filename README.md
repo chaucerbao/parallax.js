13e38bca0baf11edbe761cc2595a396b4acc722b
Alvin Chan <aschan@gmail.com>
2014-08-29 16:51:46 -0700

    Add options to set the activation point

--- README.md
+++ README.md
@@ -4,11 +4,22 @@ A Javascript parallax library that adjusts an element's background position on s
 
 ## Usage
 
-`parallax.bind(element, scale);`
+`parallax.bind(element[, options]);`
+
+```javascript
+// Defaults
+options = {
+  scale: 1,
+  anchor: 'top',
+  viewport: 'top'
+};
+```
 
 `scale` of `1` means the background position scrolls like normal, `0` means the background doesn't move.
+`anchor` and `viewport` can have values of `top`, `center`, `bottom`, or a float between 0 and 1, inclusive.
+Note: `anchor` and `viewport` work together to tell Parallax.js when to start the effect.  When the element's `anchor` reaches the viewport's `viewport` position, then start parallaxing.
 
 Example:
 ```javascript
-parallax.bind(document.getElementById('#my-element'), .5);
+parallax.bind(document.getElementById('#my-element'), { scale: .5 });
 ```
--- src/js/parallax.js
+++ src/js/parallax.js
@@ -4,41 +4,86 @@ var Parallax = (function(window, document) {
   var watchList = [],
     isInitialized = false;
 
+  /* Extend an object */
+  var extend = function(out) {
+    out = out || {};
+
+    for (var i = 1, length = arguments.length; i < length; i++) {
+      if (!arguments[i]) { continue; }
+
+      for (var key in arguments[i]) {
+        if (arguments[i].hasOwnProperty(key)) { out[key] = arguments[i][key]; }
+      }
+    }
+
+    return out;
+  };
+
   var init = function() {
     window.addEventListener('scroll', render);
   };
 
   /* Add an element to the watch list */
-  var bind = function(elements, scale) {
+  var bind = function(elements, options) {
     if (!isInitialized) {
       init();
       isInitialized = true;
     }
 
+    /* Merge the given options with defaults */
+    options = extend({
+      scale: 1,
+      anchor: 'top',
+      viewport: 'top'
+    }, options);
+
     if (!elements.length) { elements = [elements]; }
     for (var i = 0, length = elements.length; i < length; i++) {
+      /* Calculate anchor position */
+      switch (options.anchor) {
+        case 'top': options.anchor = 0; break;
+        case 'center': case 'middle': options.anchor = .5; break;
+        case 'bottom': options.anchor = 1; break;
+        default: options.anchor = parseFloat(options.anchor);
+      }
+
+      /* Calculate viewport coordinate */
+      switch (options.viewport) {
+        case 'top': options.viewport = 0; break;
+        case 'center': case 'middle': options.viewport = .5; break;
+        case 'bottom': options.viewport = 1; break;
+        default: options.viewport = parseFloat(options.viewport);
+      }
+
       watchList.push({
         element: elements[i],
-        scale: scale
+        scale: options.scale,
+        anchor: options.anchor,
+        viewport: options.viewport
       });
     }
   };
 
   /* Calculate the background position offset for each element in the watch list */
   var render = function() {
-    var element, scale, offset, y;
+    var element, options, anchor, viewport, offset, y;
 
     for (var i = 0, length = watchList.length; i < length; i++) {
       element = watchList[i].element;
-      scale = watchList[i].scale;
-      offset = window.pageYOffset - element.offsetTop;
+      options = watchList[i];
 
+      anchor = element.offsetTop + element.offsetHeight * options.anchor;
+      viewport = window.pageYOffset + window.innerHeight * options.viewport;
+
+      offset = viewport - anchor;
       if (offset < 0) {
+        /* Not at the activation point yet */
         y = 0;
-      } else if (offset > element.offsetHeight) {
+      } else if (window.pageYOffset > element.offsetTop + element.offsetHeight) {
+        /* Element has now scrolled past the top of the screen */
         continue;
       } else {
-        y = offset * (1 - scale);
+        y = offset * (1 - options.scale);
       }
 
       element.style.backgroundPosition = '0 ' + y + 'px';
